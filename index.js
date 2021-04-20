9b3c0bf9d37589742ebdd5f2ab0e1205036779df
GrzesiekKozdroj <larhendiel@yahoo.co.uk>
2021-04-20 16:16:39 +0100

    changed greeting.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT || 4200
 
 app.get('/', (req, res)=>{
-    res.send({hi:'there', user:{lala:[1,2,3,4,5], bobo:true}, ata:el=>{bobo}})
+    res.send({hi:'Larhendiel', user:{lala:[1,2,3,4,5], bobo:true}, ata:el=>{bobo}})
 })
 
 app.listen(PORT)
