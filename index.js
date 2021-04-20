c1f5d601570bb626bd192cd608cba2633bbdbae5
GrzesiekKozdroj <larhendiel@yahoo.co.uk>
2021-04-20 13:37:56 +0100

    initial commit.

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 4200
+
+app.get('/', (req, res)=>{
+    res.send({hi:'there', user:{lala:[1,2,3,4,5], bobo:true}, ata:el=>{bobo}})
+})
+
+app.listen(PORT)
