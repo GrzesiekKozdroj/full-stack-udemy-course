09086feafd1286534bdfcab08cf4eb95cf57edae
GrzesiekKozdroj <larhendiel@yahoo.co.uk>
2021-04-23 15:29:09 +0100

    mongoose added and set up.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
+require('./models/User')
 require('./services/passport')
+
 const PORT = process.env.PORT || 5000
+
+
+mongoose.connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
 
 const app = express()
 
