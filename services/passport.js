ccbc621caf53132205a437db3c0945a291adb94f
GrzesiekKozdroj <larhendiel@yahoo.co.uk>
2021-04-28 13:20:46 +0100

    added cookies.

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,17 @@ const mongoose = require('mongoose')
 const keys = require('../config/keys')
 const User = mongoose.model('users')
 
+passport.serializeUser( (user, done)=>{
+    done(null, user.id)
+} )
+
+passport.deserializeUser( (id, done)=>{
+    User.findById(id)
+        .then(
+            user=>done(null, user)
+        )
+} )
+
 passport.use( 
     new GoogleStrategy({
         clientID:keys.googleClientID,
@@ -12,7 +23,17 @@ passport.use(
         callbackURL: '/auth/google/callback'
         }, 
         (accessToken, refreshToken, profile, done)=>{
-            new User({ googleId:profile.id}).save()
+            User.findOne({ googleId: profile.id })//This method allows me to scan through database! it returns promise!!!!
+                .then((existingUser)=>{
+                    if (existingUser){
+                        //we already have a record with profile id
+                        done(null, existingUser)
+                    } else { //new user make new one!
+                        new User({ googleId:profile.id})
+                            .save()
+                            .then(user => done(null, user) )
+                    }
+                })
         }) 
     ) //new instance of authentication with google
 
