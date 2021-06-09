c5ca089ed4869bcb4e37975b08269bcb629a8439
GrzesiekKozdroj <larhendiel@yahoo.co.uk>
2021-06-09 12:18:07 +0100

    added form that needs to be changed and connected with redux.

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,12 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
+import {Link} from 'react-router-dom'
 
-class Dashboard extends Component{
-    render(){
+const Dashboard = ()=>{
         return (
             <div>
-                Header
+                Dashboard
+                <div className="fixed-action-btn">
+                <Link className="btn-floating btn-large teal" to="/surveys/new">
+                    <i className="material-icons large">add</i>
+                </Link>
+                {/* <ul>
+                    <li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a></li>
+                    <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
+                    <li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
+                    <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
+                </ul> */}
+                </div>
             </div>
         )
     }
-}
 export default Dashboard
